21ba719495025d7b08a980b79c272d5b29c7c53c
ho3eintahan <hoseintahan84@gmail.com>
2026-01-07 13:19:18 +0330

    Add main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	print('H')
+	fmt.Println("hello wowi")
+	var u user
+	u.code = 1
+	u.name = "John Doe"
+	u.email = "john.doe@example.com"
+	fmt.
+}
+
+type user struct {
+	code int
+	name string
+	email string
+	phone string
+}
